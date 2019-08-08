0e4dcf8a6a7211d1385396767aa4557a950e9888
Renaud Genevois <renaud.genevois23@gmail.com>
2019-08-08 21:27:06 +0200

    bug selection

--- src/js/editor/selection-tools.js
+++ src/js/editor/selection-tools.js
@@ -1,14 +1,21 @@
 const SELECTION_TOOLS = {};
 
 /* ({lines, selection}) =>  string  */
-export const getSelection = ({ lines, selection: { stop, start } }) =>
-	lines.map(
-		({ value }, row) =>
-			row === start.row
-				? `${value.substr(start.index, row === stop.row ? stop.index : value.length)}`
-				: row === stop.row ? value.substr(0, stop.index) : row >= start.row && row <= stop.row ? value : null
-	);
+export const getSelection = ({ lines, selection: { stop, start } }) => {
+	const copy = lines
+		.map(
+			({ value }, row) =>
+				row === start.row
+					? `${value.substr(start.index, row === stop.row ? stop.index : value.length - 1)}`
+					: row === stop.row
+						? value.substr(0, stop.index)
+						: row >= start.row && row <= stop.row ? value : null
+		)
+		.filter((t) => t !== null)
+		.join('\n');
 
+	return copy;
+};
 /* */
 SELECTION_TOOLS.getSelection = getSelection;
 
--- src/js/editor/shortcut-patterns.js
+++ src/js/editor/shortcut-patterns.js
@@ -1,91 +1,87 @@
-import * as actions from "./editor.actions";
-import { getSelection } from "./selection-tools";
+import * as actions from './editor.actions';
+import { getSelection } from './selection-tools';
 
 const SHORT_CUTS = new Map();
 
 /* */
-const unmappedPattern = pattern => () => {
-  console.debug(`unmapped pattern: ${pattern}`);
-  return false;
+const unmappedPattern = (pattern) => () => {
+	console.debug(`unmapped pattern: ${pattern}`);
+	return false;
 };
 
 /* cut to clipboard */
-const cut = () => {
-  console.log("cut");
-  return true;
+const cut = (dispatch, state) => {
+	if (state.selection) {
+		copy(dispatch, state);
+		dispatch(actions.deleteSelection());
+	}
+	return true;
 };
 
 /* paste clipboard */
 const paste = (dispatch, state) => {
-  if (navigator && navigator.clipboard)
-    navigator.clipboard.readText().then(text => {
-      if (text && text.length > 0) {
-        if (state.selection && state.selection.extentRow)
-          dispatch(actions.deleteSelection());
-        dispatch(actions.insertText(text));
-      }
-    });
-  return true;
+	if (navigator && navigator.clipboard)
+		navigator.clipboard.readText().then((text) => {
+			if (text && text.length > 0) {
+				if (state.selection && state.selection.extentRow) dispatch(actions.deleteSelection());
+				dispatch(actions.insertText(text));
+			}
+		});
+	return true;
 };
 
 /* select all */
 const selectAll = (dispatch, state) => {
-  dispatch(
-    actions.setSelection({
-      start: { row: 0, index: 0 },
-      stop: {
-        row: state.lines.length - 1,
-        index: state.lines[state.lines.length - 1].value.length
-      }
-    })
-  );
-  return true;
+	dispatch(
+		actions.setSelection({
+			start: { row: 0, index: 0 },
+			stop: {
+				row: state.lines.length - 1,
+				index: state.lines[state.lines.length - 1].value.length
+			}
+		})
+	);
+	return true;
 };
 
 /* copy to clipboard */
 const copy = (dispatch, state) => {
-  if (state.selection) {
-    const content = getSelection(state);
-    if (navigator && navigator.clipboard)
-      navigator.clipboard.writeText(content);
-  }
-  return true;
+	if (state.selection) {
+		const content = getSelection(state);
+		if (navigator && navigator.clipboard) navigator.clipboard.writeText(content);
+	}
+	return true;
 };
 
 /* MAPPING */
-SHORT_CUTS.set("ctrl|x", cut);
-SHORT_CUTS.set("ctrl|c", copy);
-SHORT_CUTS.set("ctrl|v", paste);
-SHORT_CUTS.set("ctrl|a", selectAll);
+SHORT_CUTS.set('ctrl|x', cut);
+SHORT_CUTS.set('ctrl|c', copy);
+SHORT_CUTS.set('ctrl|v', paste);
+SHORT_CUTS.set('ctrl|a', selectAll);
 
 const getPattern = ({ altKey, shiftKey, ctrlKey, key }) =>
-  `${altKey ? "alt|" : ""}${shiftKey ? "shift|" : ""}${ctrlKey ? "ctrl|" : ""}${
-    key ? key : ""
-  }`;
+	`${altKey ? 'alt|' : ''}${shiftKey ? 'shift|' : ''}${ctrlKey ? 'ctrl|' : ''}${key ? key : ''}`;
 
-const createShortcutsProvider = shortcutsMap => ({
-  get: (model = {}) => {
-    const pattern = getPattern(model);
-
-    return {
-      execute: shortcutsMap.has(pattern)
-        ? shortcutsMap.get(pattern)
-        : unmappedPattern(pattern)
-    };
-  }
+const createShortcutsProvider = (shortcutsMap) => ({
+	get: (model = {}) => {
+		const pattern = getPattern(model);
+		return {
+			execute: shortcutsMap.has(pattern) ? shortcutsMap.get(pattern) : unmappedPattern(pattern)
+		};
+	}
 });
 
 /* */
 export const composeShortcuts = (patterns = {}, erase = false) => {
-  const newMap = Object.entries(patterns).reduce((map, [pattern, action]) => {
-    if ((erase || !map.has(pattern)) && typeof action === "function") {
-      map.set(pattern, action);
-    }
+	const newMap = Object.entries(patterns).reduce((map, [ pattern, action ]) => {
+		if ((erase || !map.has(pattern)) && typeof action === 'function') {
+			map.set(pattern, action);
+		}
 
-    return map;
-  }, new Map(SHORT_CUTS));
+		return map;
+	}, new Map(SHORT_CUTS));
 
-  return createShortcutsProvider(newMap);
+	return createShortcutsProvider(newMap);
 };
 
 /* */
