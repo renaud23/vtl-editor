3f8bd09ffad1c683868ec699084a02c48c8bc591
Renaud Genevois <renaud.genevois@insee.fr>
2019-08-14 13:08:58 +0200

    scroll bar click

--- src/js/editor/components/scrollbar-up.component.js
+++ src/js/editor/components/scrollbar-up.component.js
@@ -1,14 +1,14 @@
 import React, { useContext } from "react";
 import { EditorContext } from "./editor-panel.component";
+import * as actions from "./../editor.actions";
 
 const ScrollUpDown = ({ parentEl }) => {
-  const { lines, scrollRange, selection, index, focusedRow } = useContext(
-    EditorContext
-  );
+  const state = useContext(EditorContext);
+  const { lines, scrollRange, selection, index, focusedRow, dispatch } = state;
 
   if (parentEl) {
     const dragger = parentEl && lines.length > scrollRange.offset;
-    const { height } = parentEl.getBoundingClientRect();
+    const { height, top } = parentEl.getBoundingClientRect();
     const dgHeight = Math.max((scrollRange.offset / lines.length) * height, 10);
     const dgStart = (scrollRange.start / lines.length) * height;
     return (
@@ -18,6 +18,11 @@ const ScrollUpDown = ({ parentEl }) => {
         onClick={e => {
           e.stopPropagation();
           if (dragger) {
+            const clickPosition =
+              e.clientY - top + window.scrollY - window.scrollY;
+            const percent = clickPosition / height;
+            const sr = computeScrollrange(state)(percent);
+            dispatch(actions.setScrollrange(sr));
           }
         }}
       >
@@ -57,6 +62,18 @@ const Selection = ({ start, stop, parentHeight, nbLines }) => {
 const Cursor = ({ focusedRow, parentHeight, nbLines }) => {
   const top = Math.round((focusedRow / nbLines) * parentHeight);
   return <span className="cursor" style={{ top }} />;
+};
+
+const computeScrollrange = ({ scrollRange: sr, lines }) => percent => {
+  const start = Math.min(
+    Math.round(lines.length * percent),
+    lines.length - sr.offset
+  );
+  return {
+    ...sr,
+    start,
+    stop: Math.min(start + sr.offset + 1, lines.length - 1)
+  };
 };
 
 export default ScrollUpDown;
