0014e12973d962616fbbb09e991131a720ebabcb
Renaud Genevois <renaud.genevois@insee.fr>
2019-08-14 10:59:04 +0200

    unknow

--- src/js/editor/components/scrollbar-up.component.js
+++ src/js/editor/components/scrollbar-up.component.js
@@ -5,13 +5,28 @@ const ScrollUpDown = ({ parentEl }) => {
   const { lines, scrollRange, selection, index, focusedRow } = useContext(
     EditorContext
   );
-  if (parentEl && lines.length > scrollRange.offset) {
+
+  if (parentEl) {
+    const dragger = parentEl && lines.length > scrollRange.offset;
     const { height } = parentEl.getBoundingClientRect();
     const dgHeight = Math.max((scrollRange.offset / lines.length) * height, 10);
     const dgStart = (scrollRange.start / lines.length) * height;
     return (
-      <div className="scroll-up-down" style={{ height }}>
-        <span className="dragger" style={{ height: dgHeight, top: dgStart }} />
+      <div
+        className="scroll-up-down"
+        style={{ height }}
+        onClick={e => {
+          e.stopPropagation();
+          if (dragger) {
+          }
+        }}
+      >
+        {dragger ? (
+          <span
+            className="dragger"
+            style={{ height: dgHeight, top: dgStart }}
+          />
+        ) : null}
         {selection ? (
           <Selection
             {...selection}
@@ -29,6 +44,7 @@ const ScrollUpDown = ({ parentEl }) => {
       </div>
     );
   }
+
   return null;
 };
 
